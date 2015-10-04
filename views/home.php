3c5c5bf6cb09cff89c2a266c5159ffdee720392b
Eduardo Alfonzo <eduardo.alfonzo@upr.edu>
2015-10-04 09:53:29 -0400

    home fixes

--- views/home.php
+++ views/home.php
@@ -18,6 +18,21 @@ if (isset($login)) {
         }
     }
 }
+
+function timelineCard(){
+    echo '<div class="cd-timeline-block">
+        <div class="cd-timeline-img">
+            <img src="/images/icons-svg/user-white.svg" alt="Picture">
+        </div> <!-- cd-timeline-img -->
+
+        <div class="cd-timeline-content">
+            <h2>Group Finder Team</h2>
+            <p>Our team is going to have a Happy Hour, be there... </p>
+            <a href="#0" class="cd-read-more">Read more</a>
+            <span class="cd-date">Sep 29</span>
+        </div>
+    </div> ';
+}
 ?>
 <!doctype html>
 <html lang="en"><head>
@@ -33,9 +48,10 @@ if (isset($login)) {
     <link rel="stylesheet" type="text/css" href="/css/timeline-style.css"/>
     <link rel="stylesheet" type="text/css" href="/css/reset.css"/>
     <!-- <link rel="icon" href="/images/logo.ico"> -->
+    <script type="text/javascript" src="/js/jquery.js"></script>
 </head>
 <body>
-    <!-- <script>$.material.init()</script> -->
+
     <script>//FB Log in
       // This is called with the results from from FB.getLoginStatus().
       function statusChangeCallback(response) {
@@ -179,81 +195,77 @@ if (isset($login)) {
         		</div> <!-- cd-timeline-content -->
         	</div> <!-- cd-timeline-block -->
 
-        	<div class="cd-timeline-block">
-        		<!-- ... -->
-        	</div>
+            <script>
+            $(document).ready(function () {
+              //your code here
+                $(window).scroll(function(){
+                //    console.log($(window).scrollTop() >= $(document).height() - $(window).height() - 10 );
+                    var scrollHeight = $(document).height();
+	                var scrollPosition = $(window).height() + $(window).scrollTop();
+	                if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+                        var timeline = document.getElementById("cd-timeline");
+                        var block = document.createElement("div");
+                        var att = document.createAttribute("class");
+                        att.value = "cd-timeline-block";
+                        block.setAttributeNode(att);
+
+                        var imgDiv = document.createElement("div");
+                        var att2 = document.createAttribute("class");
+                        att2.value = "cd-timeline-img";
+                        imgDiv.setAttributeNode(att2);
+
+                        var img = document.createElement("img");
+                        var att3 = document.createAttribute("src");
+                        att3.value = "/images/icons-svg/user-white.svg";
+                        img.setAttributeNode(att3);
+                        var attP = document.createAttribute("alt");
+                        attP.value = "Picture";
+                        img.setAttributeNode(attP);
+
+                        imgDiv.appendChild(img);
+                        block.appendChild(imgDiv);
+
+                        var content = document.createElement("div");
+                        var att4 = document.createAttribute("class");
+                        att4.value = "cd-timeline-content";
+                        content.setAttributeNode(att4);
+                        block.appendChild(content);
+
+                        var title = document.createElement("h2");
+                        var th2 = document.createTextNode("Title");
+                        title.appendChild(th2);
+                        content.appendChild(title);
+
+                        var desc = document.createElement("p");
+                        var tp = document.createTextNode("This is a auto get Panel");
+                        desc.appendChild(tp);
+                        content.appendChild(desc);
+
+                        //   <span class="cd-date">Sep 29</span>
+                        var date = document.createElement("span");
+                        var attD = document.createAttribute("class");
+                        attD.value = "cd-date";
+                        date.setAttributeNode(attD);
+                        dateT = document.createTextNode("Date Here");
+                        date.appendChild(dateT);
+                        content.appendChild(date);
+
+                        timeline.appendChild(block); //add the text node to the newly created div.
+
+                        // add the newly created element and its content into the DOM
+
+                        console.log("Timeline", timeline);
+                    //   document.body.insertBefore(block, currentDiv);
+
+                   }
+                })
+            });
+
+            </script>
+
+        	<!-- <div class="cd-timeline-block">
+        	</div> -->
         </section>
-
-        <!-- Sign In Modal -->
-        <div id="UserDrop" class="modal fade" role="dialog">
-            <div class="modal-dialog">
-
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="btn" class="close" data-dismiss="modal">&times;</button>
-                        <h1 class="modal-title" style="font-size:25px;">Group Finder</h1>
-                    </div>
-                    <div class="modal-body">
-                        HELLO
-                    </div>
-                    <div class="modal-footer" style="text-align:center;">
-
-                    </div>
-                </div>
-            </div>
-        </div> <!-- end of Sign In modal -->
-
-        <!-- Register Modal -->
-        <div id="RegisterM" class="modal fade" role="dialog">
-            <div class="modal-dialog">
-
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="btn" class="close" data-dismiss="modal">&times;</button>
-                        <h1 class="modal-title" style="font-size:25px;">Group Finder Registration</h1>
-                    </div>
-                    <div class="modal-body">
-                        <?php
-                            if (isset($registration)) {
-                                if ($registration->errors) {
-                                    foreach ($registration->errors as $error) {
-                                        echo '<div class="alert alert-dismissable alert-danger">'.
-                                            '<button type="button" class="close" data-dismiss="alert">×</button>'.
-                                            '<strong>'. $error .'</strong></div>' ;
-                                    }
-                                }
-                                if ($registration->messages) {
-                                    foreach ($registration->messages as $message) {
-                                        echo $message;
-                                    }
-                                }
-                            }
-                        ?>
-                        <!-- register form -->
-                        <form method="post" action="register.php" name="registerform">
-
-                            <!-- the user name input field uses a HTML5 pattern check -->
-                            <input id="login_input_username" class="login_input form-control" placeholder="Username (only letters and numbers, 2 to 64 characters)" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" style="margin: 10px 0px 10px;" required />
-
-                            <!-- the email input field uses a HTML5 email type check -->
-                            <input id="login_input_email" class="login_input form-control" placeholder="User's email" type="email" name="user_email" style="margin-bottom: 10px;" required />
-
-                            <input id="login_input_password_new" class="login_input form-control" placeholder="Password (min. 6 characters)" type="password" name="user_password_new" style="margin-bottom: 10px;" pattern=".{6,}" required autocomplete="off" />
-
-                            <input id="login_input_password_repeat" class="login_input form-control" placeholder="Repeat Password" type="password" name="user_password_repeat" style="margin-bottom: 20px;" pattern=".{6,}" required autocomplete="off" />
-                            <input class="btn btn-lg btn-success btn-block" type="submit"  name="register" value="Register" />
-
-                        </form>
-                    </div>
-                    <div class="modal-footer" style="text-align:center;">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                    </div>
-                </div>
-            </div>
-        </div> <!-- end of Sign In modal -->
-
 
 
         <footer class="footer">
@@ -263,13 +275,14 @@ if (isset($login)) {
         </footer>
     </div> <!-- /container -->
 
-
-    <script type="text/javascript" src="/js/jquery.js"></script>
+    <!-- <script src="//code.jquery.com/jquery-1.11.3.min.js"></script> -->
+    <!-- <script type="text/javascript" src="/js/jquery.js"></script> -->
     <script type="text/javascript" src="/js/bootstrap.js"></script>
     <script type="text/javascript" src="/js/material.js"></script>
     <script type="text/javascript" src="/js/ripples.js"></script>
     <script type="text/javascript" src="/js/timeline.js"></script>
     <script type="text/javascript" src="/js/modernizr.js"></script>
+    <script>$.material.init()</script>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
 </body>
