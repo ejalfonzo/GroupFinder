b8e672f458a708c3e6f31344d975297d29fa2692
ejalfonzo <eduardo.alfonzo@upr.edu>
2015-11-24 23:38:19 -0400

    Events Search
    + Fixed Filtering

--- views/Events/view.php
+++ views/Events/view.php
@@ -2,6 +2,8 @@
 if (session_id() === "" && $_SESSION['user_login_status'] != 1) { session_start(); }
 // include the configs / constants for the database connection
 require_once("../../config/db.php");
+require_once("Events.php");
+$events = new Events();
 ?>
 <!doctype html>
 <html lang="en" class="no-js">
@@ -190,34 +192,8 @@ require_once("../../config/db.php");
 
 		<div class="cd-filter">
 			<form>
-				<div class="cd-filter-block">
-					<h4>Search</h4>
 
-					<div class="cd-filter-content">
-						<input type="search" placeholder="Try panel...">
-					</div> <!-- cd-filter-content -->
-				</div> <!-- cd-filter-block -->
 
-				<div class="cd-filter-block">
-					<h4>Check boxes</h4>
-
-					<ul class="cd-filter-content cd-filters list">
-						<li>
-							<input class="filter" data-filter=".check1" type="checkbox" id="checkbox1">
-			    			<label class="checkbox-label" for="checkbox1">Option 1</label>
-						</li>
-
-						<li>
-							<input class="filter" data-filter=".check2" type="checkbox" id="checkbox2">
-							<label class="checkbox-label" for="checkbox2">Option 2</label>
-						</li>
-
-						<li>
-							<input class="filter" data-filter=".check3" type="checkbox" id="checkbox3">
-							<label class="checkbox-label" for="checkbox3">Option 3</label>
-						</li>
-					</ul> <!-- cd-filter-content -->
-				</div> <!-- cd-filter-block -->
 
 				<div class="cd-filter-block">
 					<h4>Select</h4>
@@ -226,35 +202,21 @@ require_once("../../config/db.php");
 						<div class="cd-select cd-filters">
 							<select class="filter" name="selectThis" id="selectThis">
 								<option value="">Choose an option</option>
-								<option value=".option1">Option 1</option>
-								<option value=".option2">Option 2</option>
-								<option value=".option3">Option 3</option>
-								<option value=".option4">Option 4</option>
+								<?php
+								 $categories = $events->getEventCategories();
+								//  echo("<script>console.log('results_row: ".json_encode($categories->id_category)."');</script>");
+									if($categories != null){
+										while($row = $categories->fetch_object()){
+					               			echo('<option value=".'.$row->id_category. '">'. $row->name . '</option>');
+					          			}
+								 	}
+								?>
 							</select>
 						</div> <!-- cd-select -->
 					</div> <!-- cd-filter-content -->
 				</div> <!-- cd-filter-block -->
 
-				<div class="cd-filter-block">
-					<h4>Radio buttons</h4>
-
-					<ul class="cd-filter-content cd-filters list">
-						<li>
-							<input class="filter" data-filter="" type="radio" name="radioButton" id="radio1" checked>
-							<label class="radio-label" for="radio1">All</label>
-						</li>
-
-						<li>
-							<input class="filter" data-filter=".radio2" type="radio" name="radioButton" id="radio2">
-							<label class="radio-label" for="radio2">Choice 2</label>
-						</li>
-
-						<li>
-							<input class="filter" data-filter=".radio3" type="radio" name="radioButton" id="radio3">
-							<label class="radio-label" for="radio3">Choice 3</label>
-						</li>
-					</ul> <!-- cd-filter-content -->
-				</div> <!-- cd-filter-block -->
+				
 			</form>
 
 			<a href="#0" class="cd-close">Close</a>
